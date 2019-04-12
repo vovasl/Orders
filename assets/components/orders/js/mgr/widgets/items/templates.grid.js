712a437c0a77c3deb8359e532934b1ae79998ef8
vovasl <vovasl2010@gmail.com>
2019-04-12 17:46:27 +0300

    12.04.2019

--- assets/components/orders/js/mgr/widgets/items/templates.grid.js
+++ assets/components/orders/js/mgr/widgets/items/templates.grid.js
@@ -1,14 +1,14 @@
 orders.grid.TemplatesForm = function(config) {
     config = config || {};
     Ext.applyIf(config,{
-        id: 'orders-grid-items-templates'
-        ,autoHeight: true
-        ,url: orders.config.connector_url
-        ,tbar: this.getTopBar(config)
-        ,listeners: this.getListeners(config)
-
+        id: 'orders-grid-items-templates',
+        autoHeight: true,
+        url: orders.config.connector_url,
+        tbar: new Ext.Toolbar(),
+        listeners: this.getListeners(config),
     });
     orders.grid.TemplatesForm.superclass.constructor.call(this,config);
+
 };
 
 Ext.extend(orders.grid.TemplatesForm, MODx.FormPanel,{
@@ -20,43 +20,46 @@ Ext.extend(orders.grid.TemplatesForm, MODx.FormPanel,{
             beforerender: function () {
                 this.grid = Ext.getCmp('orders-grid-items');
             },
-            afterrender: function () {
-                this.activeButton();
-            }
+            render: function () {
+                MODx.Ajax.request({
+                    url: orders.config.connector_url,
+                    params: {
+                        action: 'mgr/settings/template/getlist',
+                        sort: 'name',
+                        dir: 'asc',
+                        limit: 9999,
+                    },
+                    listeners: {
+                        success: {
+                            fn: function (data) {
+                                var templates = [];
+                                var res = data.results;
+                                for (var i = 0; i < res.length; i++) {
+                                    var button = {
+                                        id: 'but-template-' + res[i]['id'],
+                                        xtype: 'button',
+                                        text: res[i]['name'],
+                                        scope: this,
+                                        value: res[i]['id'],
+                                        handler: this.changeTemplate,
+                                    };
+                                    if(res[i]['id'] == orders.config['orders_item_template']){
+                                        Ext.applyIf(button, {
+                                            cls: 'primary-button',
+                                        });
+                                    }
+                                    templates.push(button);
+                                }
+                                this.getTopToolbar().add(new Ext.Toolbar({
+                                    renderTo: this.tbar,
+                                    items: templates,
+                                }));
+                            }, scope: this
+                        }
+                    }
+                });
+            },
         }
-    },
-
-    getTopBar: function() {
-        var template1 = {
-            id: 'but-template-7',
-            xtype: 'button',
-            text: '01_ОСНОВНОЙ',
-            scope: this,
-            value: 7,
-            handler: this.changeTemplate,
-        };
-        var template2 = {
-            id: 'but-template-6',
-            xtype: 'button',
-            text: '02_ЗАКАЗ ТП+СТАВКИ',
-            scope: this,
-            value: 6,
-            handler: this.changeTemplate,
-        };
-        var template3 = {
-            id: 'but-template-10',
-            xtype: 'button',
-            text: '03_ФРАХТ+ЯНА',
-            scope: this,
-            value: 10,
-            handler: this.changeTemplate,
-        };
-
-        var templates = [];
-        templates.push(template1);
-        templates.push(template2);
-        templates.push(template3);
-        return templates;
     },
 
     changeTemplate: function (btn) {
@@ -71,14 +74,6 @@ Ext.extend(orders.grid.TemplatesForm, MODx.FormPanel,{
     reloadPage: function() {
         location.href = location.href;
     },
-
-    activeButton: function () {
-        var btnID = 'but-template-' + orders.config['orders_item_template'];
-        var btn = Ext.getCmp(btnID);
-        if(btn) {
-            btn.addClass('primary-button');
-        }
-    }
 
 });
 Ext.reg('orders-grid-items-templates',orders.grid.TemplatesForm);
