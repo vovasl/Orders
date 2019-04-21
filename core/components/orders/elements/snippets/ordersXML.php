6283cd60a765daa533926425368db1b90bf73fc1
vovasl <vovasl2010@gmail.com>
2019-04-21 15:17:05 +0300

    21.04.2019

--- core/components/orders/elements/snippets/ordersXML.php
+++ core/components/orders/elements/snippets/ordersXML.php
@@ -1,11 +1,37 @@
 <?php
+$modx->getService('orders', 'orders', MODX_CORE_PATH . 'components/orders/model/');
+
 $output = '';
-$file = $_SERVER['DOCUMENT_ROOT'] . '/balance.xml';
+$file = $_SERVER['DOCUMENT_ROOT'] . '/Balance.xml';
 
 if (file_exists($file)) {
     $xml = simplexml_load_file($file);
     foreach($xml as $key => $val) {
-        echo $val['Партнер'] . ' - ' . $val['Баланс'] . ' - ' . $val['Сумма'] . '<br>';
+        $client = trim($val['Partner']);
+        $type = trim($val['Balance']);
+        $sum = trim($val['Sum']);
+        if($client != '') {
+            $clientDB = $modx->getObject('ordersClient', array('name' => $client));
+            if(is_object($clientDB)) {
+                $clientID = $clientDB->get('id');
+                if($type == 'Ставки'){
+                    $field = 'stavki';
+                }
+                else if($type == 'Валютные переводы'){
+                    $field = 'perevodi';
+                }
+                $items = $modx->getCollection('ordersItem', array('client' => $clientID));
+                foreach ($items as $item) {
+                    $item->set($field, $sum);
+                    $item->set('date_1c', time());
+                    $item->save();
+                }
+                //$output .= $client . ' - ' .$clientID . ' - ' . $field . ' - ' . $sum . '<br>';
+            }
+            else {
+                $output .= '<span style="color:red;">Клиент не найден:' . $client . '</span><br />';
+            }
+        }
     }
 } else {
     $output = 'Не удалось открыть файл XML';
